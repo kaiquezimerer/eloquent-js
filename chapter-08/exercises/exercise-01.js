e05272ea6dc99e45b30b4998e1f492aab1305383
Kaíque Zimerer <kaiquezimerer@outlook.com>
2021-10-19 01:32:13 -0300

    Chapter 08 - Bugs and Errors

---
+++ chapter-08/exercises/exercise-01.js
@@ -0,0 +1,26 @@
+/*
+  Chapter 08 - Bugs and Errors
+  Exercise 01 - Retry
+*/
+
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Klunk");
+  }
+}
+
+function reliableMultiply(a, b) {
+  for (;;) {
+    try {
+      return primitiveMultiply(a, b);
+    } catch (e) {
+      if (!(e instanceof MultiplicatorUnitFailure)) throw e;
+    }
+  }
+}
+
+console.log(reliableMultiply(8, 8)); // → 64
